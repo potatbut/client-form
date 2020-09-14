4646947a6adc8bb2566483b23eb06be6ce113729
potatbut <55832724+potatbut@users.noreply.github.com>
2020-09-14 15:51:17 +0300

    Name and phone inputs

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,158 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <form>
+      <div class="form-group">
+        <!-- First Name -->
+       <div class="firstName">
+          <label for="name">Имя</label>
+          <input 
+            id="firstName" 
+            type="text" 
+            class="form-control" 
+            placeholder="Имя" 
+            v-model.trim="firstName" 
+            @blur="$v.firstName.$touch()"
+          >
+          <!-- Errors for first name -->
+          <div class="error" v-if="$v.firstName.$error">
+            <template v-if="!$v.firstName.alpha">
+            Имя должно содержать только буквы
+            </template>
+            <template v-else>
+            Имя обязательно для заполнения
+            </template>
+          </div>
+        </div>
+
+        <!-- Last Name --> 
+        <div class="lastName">
+          <label for="lastName">Фамилия</label>
+          <input 
+            id="lastName" 
+            type="text" 
+            class="form-control" 
+            placeholder="Фамилия" 
+            v-model.trim="lastName"
+            @blur="$v.lastName.$touch()"
+          >
+          <!-- Errors for first name -->
+          <div class="error" v-if="$v.lastName.$error">
+            <template v-if="!$v.lastName.alpha">
+            Фамилия должна содержать только буквы
+            </template>
+            <template v-else>
+            Фамилия обязательна для заполнения
+            </template>
+          </div>
+        </div>
+
+        <!-- Patronymic -->
+        <div class="patronymic">
+          <label for="lastName">Отчество</label>
+          <input 
+            id="patronymic" 
+            type="text" 
+            class="form-control" 
+            placeholder="Отчество" 
+            v-model.trim="patronymic"
+          >
+          <div class="error" v-if="!$v.patronymic.alpha">Отчество должно содержать только буквы.</div>
+        </div>
+
+        <!-- Birthday Date -->
+        <div class="birthdayDate">
+          <label for="birthdayDate">Дата рождения</label>
+          <input 
+            id="birthdayDate" 
+            type="text" 
+            v-model="birthdayDate"
+            placeholder="дд.мм.гггг"
+            @blur="$v.birthdayDate.$touch()"
+          >
+          <!-- Errors for birthday Date -->
+          <div class="error" v-if="$v.birthdayDate.$error">
+            <template v-if="!$v.birthdayDate.validDate">
+            Дата рождения должна быть в формате дд.мм.гггг
+            </template>
+            <template v-else>
+            Фамилия обязательна для заполнения
+            </template>
+          </div>
+
+        </div>
+        <!-- Phone number  v-if="{regex: /\+7\(\d{3}\)\d{3}-\d{2}-\d{2}/}"-->
+        <div>
+          <label for="phone">Номер телефона</label>
+          <span>+7</span>
+          <input 
+            type="tel"
+            v-model="phone"
+            name="phone"
+            id="phone"
+            placeholder="(555) 555-5555"
+            autocomplete="tel"
+            maxlength="14"
+            v-phone
+            class="form-control"
+            pattern="[(][0-9]{3}[)] [0-9]{3}-[0-9]{4}"
+            required
+            >
+            <!-- id="phone"
+            type="tel"
+            v-model="phone" -->
+        </div>
+      </div>
+
+    </form>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import { alpha, required, numeric } from 'vuelidate/lib/validators'
+import moment from "moment";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      firstName: '',
+      lastName: '',
+      patronymic: '',
+      birthdayDate:'',
+      phone:'',
+    }
+  },
+  validations: {
+    firstName: {
+      alpha: val => /^[а-яё]*$/i.test(val),
+      required
+    },
+    lastName: {
+      alpha: val => /^[а-яё]*$/i.test(val),
+      required
+    },
+    patronymic: {
+      alpha: val => /^[а-яё]*$/i.test(val),
+      required
+    },
+    birthdayDate: {
+      required,
+      validDate: val => moment(val, "DD.MM.YYYY", true).isValid(),
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+ #app 
+    font-family: Avenir, Helvetica, Arial, sans-serif
+    -webkit-font-smoothing: antialiased
+    -moz-osx-font-smoothing: grayscale
+    text-align: center
+    color: #2c3e50
+    margin-top: 60px
+  
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuelidate from 'vuelidate'
+
+Vue.use(Vuelidate)
 
 Vue.config.productionTip = false
+
+Vue.directive("phone", {
+  bind(el) {
+    el.oninput = function (e) {
+      if (!e.isTrusted) {
+        return;
+      }
+
+      const x = this.value
+        .replace(/\D/g, "")
+        .match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
+      this.value = !x[2]
+        ? x[1]
+        : "(" + x[1] + ") " + x[2] + (x[3] ? "-" + x[3] : "");
+      el.dispatchEvent(new Event("input"));
+    };
+  }
+});
 
 new Vue({
   render: h => h(App),
